06c29d417d06d21d1d26fc001af6ec3dd565edb5
jmvazquezr20-eng <jmvazquezr20@gmail.com>
2026-08-17 07:56:08 -0700

    Add Perfil class with personal information

---
+++ perfil.java
@@ -0,0 +1,8 @@
+public class Perfil {
+    public static void main(String[] args) {
+        System.out.println("Nombre: JESUS MANUEL VAZQUEZ ROSAS");
+        System.out.println("Carrera: Ingeniería en Desarrollo de Software");
+        System.out.println("Pasatiempo 1: Videojuegos");
+        System.out.println("Pasatiempo 2: Escuchar música");
+    }
+}
